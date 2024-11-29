9adf4e6a3381d6a95a4d94ccb93d9eae304c2449
artemgon <artemhoncharuk123@gmail.com>
2024-11-29 21:32:35 +0200

    Task 2 is finished.  HW is executed.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,8 @@
 
 int main()
 {
+	Car car1;
+	car1.setValues(5.453, 0.146, 0, 577, 126.4, 3400, "gray", "automatic", "2024", "Rolls-Royce", "Spectre");
+	car1.printFunc();
 	return 0;
 }
--- structures.h
+++ structures.h
@@ -1,1 +1,36 @@
 #include "libs.h"
+
+struct Car
+{
+public:
+	float length, clearance, volume, power, diameter,weight;
+	string color, transmission, year, brand, model;
+	void setValues(float l, float c, float v, float p, float d, float w,
+		string col, string t, string y, string b, string m)
+	{
+		length = l; 
+		clearance = c; 
+		volume = v;
+		power = p;
+		diameter = d;
+		weight = w;
+		color = col;
+		transmission = t;
+		year = y;
+		brand = b;
+		model = m;
+	}
+	void printFunc() const {
+		cout << "Brand: " << brand << endl;
+		cout << "Model: " << model << endl;
+		cout << "Year: " << year << endl;
+		cout << "Weight: " << weight << " kg" << endl;
+		cout << "Length: " << length << " m" << endl;
+		cout << "Clearance: " << clearance << " m" << endl;
+		cout << "Engine Volume: " << volume << " L" << endl;
+		cout << "Engine Power: " << power << " HP" << endl;
+		cout << "Wheel Diameter: " << diameter << " inches" << endl;
+		cout << "Color: " << color << endl;
+		cout << "Transmission: " << transmission << endl;
+	}
+};
