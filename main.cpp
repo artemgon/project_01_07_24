4cef88acf57b4a94685158ee5daf9813160c9ff4
artemgon <artemhoncharuk123@gmail.com>
2024-11-29 15:21:26 +0200

    Task 1 is done.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 
 int main()
 {
+	Rect rect(5, 10);
+	rect.printRect();
+	rect.resizeRect(10, 10);
+	rect.printRect();
+	rect.relocateRect(5, 5);
+	rect.printRect();
 	return 0;
 }
--- structures.h
+++ structures.h
@@ -1,1 +1,61 @@
 #include "libs.h"
+
+struct Rect
+{
+public:
+	float width, length;
+    int y = 0;
+    int x = 0; 
+	Rect(float w, float l)
+	{
+		width = w;
+		length = l;
+	}
+	void printRect()
+	{
+        for (int i = 0; i < y; i++)
+        {
+            cout << endl;
+        }
+        for (int j = 0; j < x; j++)
+            cout << " "; 
+        for (int j = 0; j < width; j++)
+        {
+            cout << "-";
+        }
+        cout << endl;
+        for (int i = 1; i < length - 1; i++)
+        {
+            for (int j = 0; j < x; j++)
+                cout << " "; 
+            cout << "|";
+            for (int j = 1; j < width - 1; j++)
+            {
+                cout << " "; 
+            }
+            cout << "|" << endl;
+        }
+        for (int j = 0; j < x; j++)
+            cout << " "; 
+        for (int j = 0; j < width; j++)
+        {
+            cout << "-";
+        }
+        cout << endl;
+	}
+    void resizeRect(float w1, float l1)
+    {
+        if (w1 > 0 && l1 > 0)
+        {
+            width = w1;
+            length = l1;
+        }
+        else
+            cout << " Error..." << endl;
+    }
+    void relocateRect(int y1, int x1)
+    {
+        y = y1; 
+        x = x1;
+    }
+};
