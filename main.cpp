6277c787cee3e027ec9a4c0d6284cb877a1e7338
artemgon <artemhoncharuk123@gmail.com>
2024-11-29 20:43:53 +0200

    Task 1 is done.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 
 int main()
 {
+	ComplexNumber c_n_1(3, 4), c_n_2(1, 2);
+	cout << "Sum: ";
+	ComplexNumber::addFunc(c_n_1, c_n_2).printFunc();
+	cout << "Difference: ";
+	ComplexNumber::subFunc(c_n_1, c_n_2).printFunc();
+	cout << "Product: ";
+	ComplexNumber::multiplyFunc(c_n_1, c_n_2).printFunc();
+	cout << "Quotient: ";
+	ComplexNumber::divideFunc(c_n_1, c_n_2).printFunc();
 	return 0;
 }
--- structures.h
+++ structures.h
@@ -1,1 +1,27 @@
 #include "libs.h"
+
+struct ComplexNumber
+{
+	float re, im;
+	ComplexNumber(float r_p, float i_p) : re(r_p), im(i_p) {}
+	ComplexNumber static addFunc(const ComplexNumber& c_n_1, const ComplexNumber& c_n_2)
+	{
+		return ComplexNumber(c_n_1.re + c_n_2.re, c_n_1.im + c_n_2.im);
+	}
+	ComplexNumber static subFunc(const ComplexNumber& c_n_1, const ComplexNumber& c_n_2)
+	{
+		return ComplexNumber(c_n_1.re - c_n_2.re, c_n_1.im - c_n_2.im);
+	}
+	ComplexNumber static multiplyFunc(const ComplexNumber& c_n_1, const ComplexNumber& c_n_2) {
+		return ComplexNumber((c_n_1.re * c_n_2.re) - (c_n_1.im * c_n_2.im), (c_n_1.re * c_n_2.im) + (c_n_1.im * c_n_2.re));
+	}
+	ComplexNumber static divideFunc(const ComplexNumber& c_n_1, const ComplexNumber& c_n_2) {
+		float denominator = c_n_2.re * c_n_2.re + c_n_2.im * c_n_2.im; 
+		return ComplexNumber((c_n_1.re * c_n_2.re + c_n_1.im * c_n_2.im) / denominator,
+		(c_n_1.im * c_n_2.re - c_n_1.re * c_n_2.im) / denominator);
+	}
+	void printFunc() const
+	{
+		cout << re << " + " << im << " * i" << endl;
+	}
+};
